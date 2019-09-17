daa4057fae41e123a40036eb0c117e40c4b86ce2
Robin Schlenker <robin.schlenker@codecentric.de>
2019-09-17 22:30:29 +0200

    initial setup with first cms call

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <md-app id="app" md-mode="reveal">
+    <md-app-toolbar class="md-primary">
+      <div class="md-toolbar-row">
+        <md-tabs class="md-primary">
+          <md-tab id="tab-home" md-label="Home" to="/">
+          </md-tab>
+          <md-tab id="tab-pages" md-label="Blog" to="/blogs"></md-tab>
+          <md-tab id="tab-posts" md-label="Photos" to="/photos"></md-tab>
+        </md-tabs>
+      </div>
+    </md-app-toolbar>
+
+    <md-app-content>
+      <router-view />
+    </md-app-content>
+  </md-app>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/components/BlogOverview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Welcome on the blog Overview
+  </div>
+</template>
+
+<script>
+import Strapi from 'strapi-sdk-javascript/build/main'
+
+export default {
+  name: 'BlogOverview',
+  mounted () {
+    const strapi = new Strapi('http://schlenker.io:1337')
+    strapi.getEntries('blogs').then((result) => {
+      console.log(result)
+    })
+  }
+}
+</script>
