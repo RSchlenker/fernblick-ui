32884b7cc5cd3f40d5232f2a6c0962e7e4e5aee8
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-19 08:18:07 +0200

    First Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM nginx:1.15.8
+
+COPY ./dist /usr/share/nginx/html
+
+EXPOSE 80
+
--- src/components/BlogOverview.vue
+++ src/components/BlogOverview.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    Welcome on the blog Overview
+    <h2>Welcome on the blog Overview</h2>
+    <md-card v-for="blog in blogs">
+      <md-card-header>
+        <div class="md-title">{{blog.title}}</div>
+      </md-card-header>
+
+      <md-card-content>
+        {{blog.text}}
+        <md-card-media>
+          <img :src="'http://schlenker.io:1337' + blog['preview-image'].url">
+        </md-card-media>
+      </md-card-content>
+    </md-card>
   </div>
 </template>
 
@@ -11,9 +23,14 @@ export default {
   name: 'BlogOverview',
   mounted () {
     const strapi = new Strapi('http://schlenker.io:1337')
-    strapi.getEntries('blogs').then((result) => {
-      console.log(result)
+    strapi.getEntries('blogs').then((blogs) => {
+      this.blogs = this.blogs.concat(blogs)
     })
+  },
+  data () {
+    return {
+      blogs: []
+    }
   }
 }
 </script>
