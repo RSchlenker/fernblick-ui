f90ff87e23e6bb17e85723e1812e56928ab4ce75
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-20 17:27:50 +0200

    Test traefik

--- build.sh
+++ build.sh
@@ -4,11 +4,10 @@ set +x
 set +e
 set +v
 
-export OLD_COMMIT_SHA=$(git rev-parse HEAD^1)
 export COMMIT_SHA=$(git rev-parse HEAD)
 
 docker build -t fernblick-ui:$COMMIT_SHA .;
-docker stop fernblick-$OLD_COMMIT_SHA;
+docker stop $(docker ps --format '{{.Names}}' | grep fernblick-ui-);
 
-docker run -d --name fernblick-$COMMIT_SHA -p 0.0.0.0:80:80 fernblick-ui:$COMMIT_SHA;
+docker run -d --name fernblick-ui-$COMMIT_SHA -p 0.0.0.0:80:80 fernblick-ui:$COMMIT_SHA;
 
