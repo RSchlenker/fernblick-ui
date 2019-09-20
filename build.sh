d46b695bd3fa7fd3c683155373c5a3bb0801f20a
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-20 17:10:19 +0200

    stop old container before starting new one

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ export OLD_COMMIT_SHA=$(git rev-parse HEAD^1)
 export COMMIT_SHA=$(git rev-parse HEAD)
 
 docker build -t fernblick-ui:$COMMIT_SHA .;
+docker stop fernblick-$OLD_COMMIT_SHA;
+
 docker run -d --name fernblick-$COMMIT_SHA -p 0.0.0.0:80:80 fernblick-ui:$COMMIT_SHA;
 
-docker stop fernblick-$OLD_COMMIT_SHA;
