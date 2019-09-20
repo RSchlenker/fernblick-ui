a7b0783997bac3e7600b621bb85cd38d443083b2
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-20 17:56:55 +0200

    Use randum string in container name

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ set +e
 set +v
 
 export COMMIT_SHA=$(git rev-parse HEAD)
+export RAND_TAG_NAME=$(head /dev/urandom | tr -dc a-z0-9 | head -c 13)
 
 docker build -t fernblick-ui:$COMMIT_SHA .;
-docker rm --force fernblick-ui;
+docker stop $(docker ps -q -f name=fernblick-ui-);
 
-docker run -d -p 0.0.0.0:80:80 --name=fernblick-ui fernblick-ui:$COMMIT_SHA;
+docker run -d -p 0.0.0.0:80:80 --name=fernblick-ui-$RAND_TAG_NAME fernblick-ui:$COMMIT_SHA;
 
