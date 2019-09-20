2584ac7765c2eab5fbb544081814450087deac08
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-20 17:09:06 +0200

    Don't build twice

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ export DEPLOYMENT_USER=deleadon
 export DEPLOYMENT_HOST=schlenker.io
 
 echo 'updating git repository'
-ssh $DEPLOYMENT_USER@$DEPLOYMENT_HOST -i ~/.ssh/contabo 'cd ~/projects/fernblick/fernblick-ui/; git pull origin master; ./build.sh'
+ssh $DEPLOYMENT_USER@$DEPLOYMENT_HOST -i ~/.ssh/contabo 'cd ~/projects/fernblick/fernblick-ui/; git pull origin master;'
 
 echo 'starting build and deployment'
 ssh $DEPLOYMENT_USER@$DEPLOYMENT_HOST -i ~/.ssh/contabo 'cd ~/projects/fernblick/fernblick-ui/; ./build.sh'
