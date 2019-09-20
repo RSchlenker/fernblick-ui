e60b5f8722dec9350e3c6d285669c31e536438e3
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-20 17:03:30 +0200

    first deployment scripts

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set +x
+set +e
+set +v
+
+export OLD_COMMIT_SHA=$(git rev-parse HEAD^1)
+export COMMIT_SHA=$(git rev-parse HEAD)
+
+docker build -t fernblick-ui:$COMMIT_SHA .;
+docker run -d --name fernblick-$COMMIT_SHA -p 0.0.0.0:80:80 fernblick-ui:$COMMIT_SHA;
+
+docker stop fernblick-$OLD_COMMIT_SHA;
---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export DEPLOYMENT_USER=deleadon
+export DEPLOYMENT_HOST=schlenker.io
+
+echo 'updating git repository'
+ssh $DEPLOYMENT_USER@$DEPLOYMENT_HOST -i ~/.ssh/contabo 'cd ~/projects/fernblick/fernblick-ui/; git pull origin master; ./build.sh'
+
+echo 'starting build and deployment'
+ssh $DEPLOYMENT_USER@$DEPLOYMENT_HOST -i ~/.ssh/contabo 'cd ~/projects/fernblick/fernblick-ui/; ./build.sh'
