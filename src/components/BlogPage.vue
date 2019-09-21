3fa64af13627590cf32596af669bd360d3f82ad1
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-21 10:25:57 +0200

    Enable rich text format for blogs

--- src/components/BlogPage.vue
+++ src/components/BlogPage.vue
@@ -2,18 +2,21 @@
   <div v-if="showNotFound">The Blog you are looking for couldn't be found</div>
   <div v-else>
     <h1>{{blog.title}}</h1>
-    <p>{{blog.text}}</p>
-    <div v-for="picture in blog.pictures" :key="picture.id">
-      <img :src="'http://strapi.schlenker.io' + picture.url">
-    </div>
+    <hr/>
+    <vue-markdown :source="blog.text"></vue-markdown>
+<!--    <div v-for="picture in blog.pictures" :key="picture.id">-->
+<!--      <img :src="'http://strapi.schlenker.io' + picture.url">-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
 import Strapi from 'strapi-sdk-javascript/build/main'
+import VueMarkdown from 'vue-markdown'
 
 export default {
   name: 'BlogPage',
+  components: {VueMarkdown},
   data () {
     return {
       blog: {},
@@ -31,8 +34,13 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 p {
   font-size: 1.3em;
+  line-height: 1.4em;
+}
+li {
+  font-size: 1.3em;
+  line-height: 1.4em;
 }
 </style>
