6e632022f03014db343045c3f73d6c2253afe2d9
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-22 14:19:16 +0200

    Initial shortoverview layout WIP

--- src/components/BlogPage.vue
+++ src/components/BlogPage.vue
@@ -1,8 +1,9 @@
 <template>
   <div v-if="errorWhileLoadingBlog">The Blog you are looking for couldn't be found</div>
-  <div v-else>
+  <div v-else class="blog-wrapper">
     <h1>{{activeBlog.title}}</h1>
     <hr/>
+    <blog-short-overview :data="activeBlog.shortoverview" />
     <vue-markdown :source="activeBlog.text"></vue-markdown>
   </div>
 </template>
@@ -10,10 +11,11 @@
 <script>
 import VueMarkdown from 'vue-markdown'
 import { mapGetters, mapActions } from 'vuex'
+import BlogShortOverview from '@/components/BlogShortOverview'
 
 export default {
   name: 'BlogPage',
-  components: {VueMarkdown},
+  components: {BlogShortOverview, VueMarkdown},
   mounted () {
     this.loadBlog(this.$route.params.id)
   },
@@ -34,5 +36,11 @@ p {
 li {
   font-size: 1.3em;
   line-height: 1.4em;
+}
+
+@media only screen and (min-width: 600px) {
+  .blog-wrapper {
+    padding: 0 10em;
+  }
 }
 </style>
---
+++ src/components/BlogShortOverview.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div>Auf einen Blick:</div>
+    <div class="overview-wrapper">
+      <div class="card short-info">
+        <div class="card-body donut-card-body">
+          <vc-donut :size="50" unit="px" :sections="timeNeeded" :total="12 * 60">{{data['time-needed']}}min</vc-donut>
+        </div>
+      </div>
+      <div class="card short-info">
+        <div class="card-body donut-card-body">
+          <vc-donut :size="50" unit="px" :sections="timeRecommended" :total="12 * 60">{{data['time-recommended']}}min</vc-donut>
+        </div>
+      </div>
+      <div class="card short-info">
+        <div class="card-body">
+        </div>
+      </div>
+      <div class="card short-info">
+        <div class="card-body">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Donut from 'vue-css-donut-chart'
+import 'vue-css-donut-chart/dist/vcdonut.css'
+
+Vue.use(Donut)
+
+export default {
+  name: 'BlogShortOverview',
+  props: {
+    data: {
+      required: true
+    }
+  },
+  data () {
+    return {
+      timeNeeded: [{ value: this.data['time-needed'], color: '#17a2b8' }],
+      timeRecommended: [{ value: this.data['time-recommended'], color: '#2eb859' }]
+    }
+  }
+}
+</script>
+
+<style>
+.overview-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: start;
+}
+.short-info {
+  height: 5em;
+  width: 5em;
+}
+.donut-card-body {
+  padding-top: 0.6em;
+}
+/* Donut chart text size */
+.cdc-text {
+  font-size: 0.8em !important;
+}
+</style>
