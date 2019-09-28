bc617dc02badb8b71498e9d5246f2941e95119bb
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-28 14:23:54 +0200

    Introduce map in home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,47 @@
 <template>
   <div>
-    <h1>Welcome on our blog!</h1>
-    <p>This is obviously work in progress!</p>
+    <h1>Willkommen bei Fernblick</h1>
+    <l-map ref="blogmap" class="blogmap mt-4" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url"></l-tile-layer>
+      <l-marker :lat-lng="markerLatLng">
+        <l-icon
+          :icon-size="iconSize"
+          icon-url="https://fonts.gstatic.com/s/i/materialicons/golf_course/v1/24px.svg?download=true" >
+        </l-icon>
+      </l-marker>
+    </l-map>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+  import {LMap, LTileLayer, LMarker, LIcon} from 'vue2-leaflet'
+  import 'leaflet/dist/leaflet.css'
+
+  export default {
+    name: 'Home',
+    components: {LMap, LTileLayer, LMarker, LIcon},
+    data () {
+      return {
+        url: 'https://tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png?apikey=7dff32227b3b423bb5835d9e9d536c35',
+        zoom: 3,
+        center: [47.313220, -1.319482],
+        markerLatLng: [47.313220, -1.319482],
+        iconSize: [32, 37],
+        iconAnchor: [16, 37]
+      }
+    },
+    mounted () {
+      this.$nextTick(() => {
+        this.$refs.blogmap.mapObject.scrollWheelZoom.disable();
+      })
+    },
+  }
 </script>
+<style>
+  .blogmap {
+    position: absolute;
+    left: 0;
+    right: 0;
+    height: 400px !important;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import App from './App'
 import router from './router'
 import store from './components/store/index'
 import VueMaterialIcon from 'vue-material-icon'
+import { Icon }  from 'leaflet'
+
+// this part resolve an issue where the markers would not appear
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 Vue.component(VueMaterialIcon.name, VueMaterialIcon)
 Vue.config.productionTip = false
