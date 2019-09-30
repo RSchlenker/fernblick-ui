c9cd06377d3155b1cfcf7e72322fcc54a1bc20e8
Robin Schlenker <robin.schlenker10.10@gmail.com>
2019-09-30 11:44:29 +0200

    Migrate to brimborium.org

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ export RAND_TAG_NAME=$(head /dev/urandom | tr -dc a-z0-9 | head -c 13)
 yarn install
 yarn build
 
-docker build -t fernblick-ui:$COMMIT_SHA .;
+docker build -t brimborium-ui:$COMMIT_SHA .;
 docker stop $(docker ps -q -f name=fernblick-ui-);
 
-docker run -d -p 0.0.0.0:7890:80 --label="traefik.http.routers.fernblick.rule=Host(\`fernblick.schlenker.io\`)" --name=fernblick-ui-$RAND_TAG_NAME fernblick-ui:$COMMIT_SHA;
+docker run -d -p 0.0.0.0:7890:80 --label="traefik.http.routers.brimborium.rule=Host(\`brimborium.org\`)" --name=brimborium-ui-$RAND_TAG_NAME fernblick-ui:$COMMIT_SHA;
 
